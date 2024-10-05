ed6858d8f77df1d30aa75e9777e744d5d509c31c
AlexJKL <alexandr_sidorenko@icloud.com>
2024-10-05 23:33:15 +0300

    T2: L,R,D.

--- tetris/tetris2.py
+++ tetris/tetris2.py
@@ -3,8 +3,8 @@ import pygame, os
 
 # Game setup
 cell_size = 20
-board_w = 10
-board_h = 10 # 3 lines for figure creating
+board_w = 20
+board_h = 20 # 3 lines for figure creating
 
 # pygame setup
 pygame.init()
@@ -13,17 +13,17 @@ clock = pygame.time.Clock()
 running = True
 
 # Game variables
-isNeedFigure = True
+is_figure_need = True
 
 game_field = [[0 for _ in range(board_h)] for _ in range(board_w)]
 
 def Create_figure():
-    global isNeedFigure, figure_coords, game_field
-    if isNeedFigure == True:
+    global is_figure_need, figure_coords, game_field
+    if is_figure_need == True:
         figure_coords = [[0,5], [0,6], [1,5], [1,6], [1,7]]
         for el in figure_coords:
             game_field[el[0]][el[1]] = 1
-        isNeedFigure = False
+        is_figure_need = False
     
 def Move_figure_left():
     global figure_coords, game_field
@@ -64,12 +64,37 @@ def Move_figure_right():
             figure_coords[i][1] += 1
 
 def Move_figure_down():
-    print("Down")
+    global figure_coords, game_field, is_figure_need
+    can_move = True
+
+    for el in figure_coords:
+        if el[0] + 1 >= board_h or (game_field[el[0]+1][el[1]] == 2):
+            for el in figure_coords:
+                game_field[el[0]][el[1]] = 2
+            is_figure_need = True
+            return
+            
+    if can_move == True:
+        for el in figure_coords:
+            game_field[el[0]][el[1]] = 0
+
+        for el in figure_coords:
+            game_field[el[0]+1][el[1]] = 1
+
+        for i in range(len(figure_coords)):
+            figure_coords[i][0] += 1
 
 def Draw_game_field():
     os.system('cls')
     for row in game_field:
         print(row)
+    
+    for i in range(len(game_field)):
+        for j in range(len(game_field[i])):
+            if game_field[j][i] == 1:
+                pygame.draw.rect(screen, "blue", (i * cell_size, j * cell_size, cell_size, cell_size))
+            if game_field[j][i] == 2:
+                pygame.draw.rect(screen, "red", (i * cell_size, j * cell_size, cell_size, cell_size))
 
 while running:
     # poll for events
